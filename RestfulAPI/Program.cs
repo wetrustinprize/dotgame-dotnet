225b8a64b013b5971d61612d34b351ee29d5c04d
Prize <me@wetrustinprize.com>
2023-05-15 17:16:26 -0300

    fix(restful): lowercase URL

--- RestfulAPI/Program.cs
+++ RestfulAPI/Program.cs
@@ -20,6 +20,7 @@ builder.Host.UseOrleans(siloBuilder =>
     siloBuilder.AddMemoryGrainStorageAsDefault();
 });
 
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
