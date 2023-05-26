bd5dab7d84e640288e8805652114867dc0bbfa8f
Prize <me@wetrustinprize.com>
2023-05-26 12:19:36 -0300

    feat(orleans/test): new tests
    BREAKING CHANGE: not working with Json serialization, will fix l8r

--- DotGameOrleans_Test/LobbyTests/LobbyTests.cs
+++ DotGameOrleans_Test/LobbyTests/LobbyTests.cs
@@ -27,4 +27,43 @@ public class LobbyTests
         
         Assert.AreEqual(ownerGuid, await lobbyGrain.GetOwner());
     }
+    
+    [TestMethod]
+    [ExpectedException(typeof(LobbyNotInitialized))]
+    public async Task NewLobby_ShouldNotBeInitialized()
+    {
+        var lobbyGrain = _cluster.GrainFactory.GetGrain<ILobbyGrain>(Guid.NewGuid());
+        
+        await lobbyGrain.GetState();
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(LobbyAlreadyJoined))]
+    public async Task JoinLobby_AlreadyJoined()
+    {
+        var sessionGuid = Guid.NewGuid();
+        var lobbyGrain = _cluster.GrainFactory.GetGrain<ILobbyGrain>(Guid.NewGuid());
+        
+        await lobbyGrain.Init(Guid.NewGuid(), 4, 4);
+        lobbyGrain.AddPlayer(sessionGuid);
+        lobbyGrain.AddPlayer(sessionGuid);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(LobbyInProgress))]
+    public async Task JoinLobby_LobbyStarted()
+    {
+        throw new NotImplementedException();
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(NotInLobby))]
+    public async Task LeaveLobby_NotJoined()
+    {
+        var sessionGuid = Guid.NewGuid();
+        var lobbyGrain = _cluster.GrainFactory.GetGrain<ILobbyGrain>(Guid.NewGuid());
+        
+        await lobbyGrain.Init(Guid.NewGuid(), 4, 4);
+        lobbyGrain.RemovePlayer(sessionGuid);
+    }
 }
--- DotGameOrleans_Test/TestSiloConfigurator.cs
+++ DotGameOrleans_Test/TestSiloConfigurator.cs
@@ -1,13 +1,19 @@
+using Orleans.Serialization;
 using Orleans.TestingHost;
 
 namespace DotGameOrleans_Test;
 
 public class TestSiloConfigurator : ISiloConfigurator
 {
-    public void Configure(ISiloBuilder hostBuilder)
+    public void Configure(ISiloBuilder siloBuilder)
     {
-        hostBuilder
-            .UseLocalhostClustering()
-            .AddMemoryGrainStorageAsDefault();
+        siloBuilder.UseLocalhostClustering();
+        siloBuilder.AddMemoryGrainStorageAsDefault();
+
+        siloBuilder.Services.AddSerializer(serializerBuilder =>
+        {
+            serializerBuilder.AddJsonSerializer(isSupported: type =>
+                type.Namespace != null && type.Namespace.StartsWith("DotGameLogic"));
+        });
     }
 }
