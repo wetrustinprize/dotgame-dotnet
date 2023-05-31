2f0e689676b012388afc17e9ba09fac7ce22a1fc
Prize <me@wetrustinprize.com>
2023-05-31 18:56:29 -0300

    feat(orleans): new methods to the GameGrain

--- DotGameOrleans/Grains/Game/GameGrain.cs
+++ DotGameOrleans/Grains/Game/GameGrain.cs
@@ -4,12 +4,44 @@ namespace DotGameOrleans.Grains.Game;
 
 public class GameGrain : Grain<GameGrainState>, IGameGrain
 {
-    public Task Init(IEnumerable<Guid> players, int height, int width)
+    #region Checkers
+
+    private void CheckInitialized()
     {
+        if (!State.Initialized)
+        {
+            throw new GameNotInitializedException(this.GetPrimaryKey());
+        }
+    }
+
+    /// <summary>
+    /// Checks if a session is in the Players list and has not left the game.
+    /// </summary>
+    /// <param name="session">The session to check</param>
+    /// <exception cref="NotInGameException">If the session does not exist or has left</exception>
+    private void CheckInGame(Guid session)
+    {
+        var exists = State.Players.Any(player => player.Session == session);
+        var left = State.Players.Any(player => player.Session == session && player.Left);
+
+        if (!exists || left)
+            throw new NotInGameException(this.GetPrimaryKey(), session);
+    }
+
+    #endregion
+
+    public Task Init(IEnumerable<Guid> players, Guid owner, int height, int width)
+    {
+        var playersList = players.ToList();
+
+        if (playersList.All(player => player != owner))
+            throw new OwnerNotInGameException(this.GetPrimaryKey(), owner);
+
         State = new GameGrainState
         {
             Initialized = true,
-            Players = players.Select(session => new GamePlayer
+            Owner = owner,
+            Players = playersList.Select(session => new GamePlayer
             {
                 Session = session
             }).ToList(),
@@ -19,4 +51,18 @@ public class GameGrain : Grain<GameGrainState>, IGameGrain
 
         return Task.CompletedTask;
     }
+
+    public Task<GameGrainState> GetState()
+    {
+        CheckInitialized();
+
+        return Task.FromResult(State);
+    }
+
+    public Task RemoveSession(Guid session)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> IsOwner(Guid session) => Task.FromResult(session == State.Owner);
 }
--- DotGameOrleans/Grains/Game/IGameGrain.cs
+++ DotGameOrleans/Grains/Game/IGameGrain.cs
@@ -1,14 +1,16 @@
 using DotGameLogic;
+using DotGameLogic.Exceptions;
 
 namespace DotGameOrleans.Grains.Game;
 
 [GenerateSerializer]
-public class GameGrainState
+public record GameGrainState
 {
     [Id(0)] public bool Initialized { get; init; }
     [Id(1)] public List<GamePlayer> Players { get; init; } = new();
     [Id(2)] public int CurrentPlayerIndex { get; set; } = -1;
     [Id(3)] public Board Board { get; init; } = null!;
+    [Id(4)] public Guid Owner { get; init; } = Guid.Empty;
 }
 
 public interface IGameGrain : IGrainWithGuidKey
@@ -17,9 +19,29 @@ public interface IGameGrain : IGrainWithGuidKey
     /// Initializes the game
     /// </summary>
     /// <param name="players">The players of the game</param>
+    /// <param name="owner">The owner of this game</param>
     /// <param name="height">The height of the board</param>
     /// <param name="width">The width of the board</param>
     /// <returns></returns>
-    /// <exception cref="InvalidGameBoardSize">Inputted board size is invalid</exception>
-    public Task Init(IEnumerable<Guid> players, int height, int width);
+    /// <exception cref="InvalidBoardSize">Inputted board size is invalid</exception>
+    public Task Init(IEnumerable<Guid> players, Guid owner, int height, int width);
+
+    /// <summary>
+    /// Gets the current state of the game
+    /// </summary>
+    /// <returns>The current state of the game</returns>
+    public Task<GameGrainState> GetState();
+
+    /// <summary>
+    /// Removes a session from the game
+    /// </summary>
+    /// <param name="session"></param>
+    public Task RemoveSession(Guid session);
+
+    /// <summary>
+    /// Check if is the owner of the game
+    /// </summary>
+    /// <param name="session">The session to check the ownership</param>
+    /// <returns>True if is the owner</returns>
+    public Task<bool> IsOwner(Guid session);
 }
