ba56bf96c404bfadb2841a4641f8c46f96e08159
Prize <me@wetrustinprize.com>
2023-06-16 16:19:42 -0300

    refac!: Removed Orleans, the RestfulAPI will need to be rebuilt from ground up

--- DotGameOrleans/Grains/Lobby/LobbyStateEnum.cs
+++
@@ -1,8 +0,0 @@
-namespace DotGameOrleans.Grains.Lobby;
-
-public enum LobbyStateEnum
-{
-    WaitingForPlayers,
-    InProgress,
-    Finished
-}
--- DotGameOrleans_Test/Cluster/ClusterCollection.cs
+++
@@ -1,7 +0,0 @@
-namespace DotGameOrleans_Test.Cluster;
-
-[CollectionDefinition(ClusterCollection.Name)]
-public class ClusterCollection : ICollectionFixture<ClusterFixture>
-{
-    public const string Name = "ClusterCollection";
-}
